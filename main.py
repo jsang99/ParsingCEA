93e4a4857f128067ce5976d43897f263df534a74
jsang99 <60195598+jsang99@users.noreply.github.com>
2022-06-07 18:35:22 -0400

    ksks brys'

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 import os
+import numpy as np
 
 def parse(filename):
     with open(filename) as f:  # opening a file
         lines = f.readlines()  # creating an array called lines with lines in that mf
-    print(lines)
-    
+
+    AEAT = []
+    CSTAR = []
+    CF = []
+    Ivac = []
+    Isp = []
+
+    for line in lines:
+        if "Ae/At" in line:
+            new = float(line.replace('Ae/At','').replace(' ','').replace('\n',''))
+            AEAT.append(new)
+        if "CSTAR, FT/SEC" in line:
+            new = float(line.replace('CSTAR, FT/SEC', '').replace(' ', '').replace('\n', ''))
+            CSTAR.append(new)
+        if "CF" in line:
+            new = float(line.replace('CF', '').replace(' ', '').replace('\n', ''))
+            CF.append(new)
+        if "Ivac,LB-SEC/LB" in line:
+            new = float(line.replace('Ivac,LB-SEC/LB', '').replace(' ', '').replace('\n', ''))
+            Ivac.append(new)
+        if "Isp, LB-SEC/LB " in line:
+            new = float(line.replace('Isp, LB-SEC/LB', '').replace(' ', '').replace('\n', ''))
+            Isp.append(new)
+
+    print("AEAT ", len(AEAT))
+    print("CSTAR ", len(CSTAR))
+    print("CF ", len(CF))
+    print("Ivac ", len(Ivac))
+    print("Isp ", len(Isp))
+
 
 
 
