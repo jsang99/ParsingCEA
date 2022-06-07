4e211992ef9a5b471c66e1cd0abf013c5a9140fa
jsang99 <60195598+jsang99@users.noreply.github.com>
2022-06-07 17:53:13 -0400

    Output file upload

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+import os
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def parse(filename):
+    with open(filename) as f:  # opening a file
+        lines = f.readlines()  # creating an array called lines with lines in that mf
+    print(lines)
+    
 
 
-# Press the green button in the gutter to run the script.
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    parse('sample_output.out')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
