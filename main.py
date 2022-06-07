34a7946365508ea4450bcab6717bc9f45d057c6a
jsang99 <60195598+jsang99@users.noreply.github.com>
2022-06-07 19:55:45 -0400

    newest ksksksks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def parse(filename):
     with open(filename) as f:  # opening a file
         lines = f.readlines()  # creating an array called lines with lines in that mf
@@ -14,10 +15,9 @@ def parse(filename):
     pressures = []
     ofus = []
 
-
     for line in lines:
         if "Ae/At" in line:
-            new = float(line.replace('Ae/At','').replace(' ','').replace('\n',''))
+            new = float(line.replace('Ae/At', '').replace(' ', '').replace('\n', ''))
             AEAT.append(new)
         if "CSTAR, FT/SEC" in line:
             new = float(line.replace('CSTAR, FT/SEC', '').replace(' ', '').replace('\n', ''))
@@ -32,14 +32,14 @@ def parse(filename):
             new = float(line.replace('Isp, LB-SEC/LB', '').replace(' ', '').replace('\n', ''))
             Isp.append(new)
         if "p,psia" in line:
-            pressures = line.replace('p,psia=','').replace(' ','').replace('\n','').split(',')
+            pressures = line.replace('p,psia=', '').replace(' ', '').replace('\n', '').split(',')
             i = 0
             while i < len(pressures):
                 temp = float(pressures[i])
                 pressures[i] = temp
                 i += 1
         if "o/f=" in line:
-            ofus = line.replace('o/f=','').replace(' ','').replace('\n','').split(',')
+            ofus = line.replace('o/f=', '').replace(' ', '').replace('\n', '').split(',')
             i = 0
             while i < len(ofus):
                 temp = float(ofus[i])
@@ -47,27 +47,35 @@ def parse(filename):
                 i += 1
 
     numOFUS = len(ofus)
-    numPRESSURES= len(pressures)
+    numPRESSURES = len(pressures)
 
     ofus = list(np.repeat(ofus, numPRESSURES))
-    print(ofus)
+    #print(ofus)
     pressures = pressures * numOFUS
-    print(pressures)
+    #print(pressures)
 
-    print("AEAT ", len(AEAT))
-    print("CSTAR ", len(CSTAR))
-    print("CF ", len(CF))
-    print("Ivac ", len(Ivac))
-    print("Isp ", len(Isp))
-    print("OFUS ", len(ofus))
-    print("pressures ", len(pressures))
+    #print("AEAT ", len(AEAT))
+    #print("CSTAR ", len(CSTAR))
+    #print("CF ", len(CF))
+    #print("Ivac ", len(Ivac))
+    #print("Isp ", len(Isp))
+    #print("OFUS ", len(ofus))
+    #print("pressures ", len(pressures))
 
 
+    # data to be plotted
+    x = pressures[:numOFUS]
+    y = Isp[:numOFUS]
 
+    # plotting
+    plt.title("Line graph")
+    plt.xlabel("X axis")
+    plt.ylabel("Y axis")
+    plt.plot(x, y, color="red")
+    plt.show()
 
 
 if __name__ == '__main__':
     parse('sample_output.out')
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
